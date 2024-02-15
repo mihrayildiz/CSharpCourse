2b5e1573141253217b9f37ae1ed7acb7cc988c1d
mihrayildiz <mihrayildiz@hotmail.com>
2024-02-16 02:08:24 +0300

    week 2 Work 2

---
+++ Constructors/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Customer customer = new Customer { Id =1, FirstName="Engin",City ="Ankara"};//constructor çalışır.
+
+Customer customer1 = new Customer(2, "Derin", "Demiroğ", "Ankara");
+Console.WriteLine(customer1.FirstName);
+
+Customer customer2 = new Customer();
+customer2.Id = 2;
+
+
+class Customer
+{
+    //default constructor
+    public Customer()
+    {
+        //customer ve customer2 da çalışır.
+    }
+    public Customer(int id, string firstName, string lastName, string city) //default constructor ezildi.
+    {
+        Id = id; //parametreli method için çalışınca döngüye girecek o  halde customer1 in değerleri aması için burada set etmelisin.
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+    }
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+}
---
+++ ReferencesTypes/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Person person1 = new Person();
+Person person2 = new Person();
+
+person1.FirstName = "Engin";
+person2 = person1; //aslında addres atadım. çünkü referans türlü değişken
+person1.FirstName = "Derin"; //artık person2 nin de firstname i derin olur.
+Console.WriteLine(person2.FirstName);
+
+
+Customer customer = new Customer();
+customer.FirstName = "Salih";
+customer.CreditCardNumber = "123456789";
+    //değeri gelir çünkü direk customer üzerinden erişmeye çalışıyorum.
+Employee employee = new Employee();
+employee.FirstName = "Veli";
+//customer = employee; asla denilemez int ve stringi birbirine atayamazsın
+
+Person person3 = customer; //customer bir persondır. Person temel sınftır.
+Console.WriteLine(person3.FirstName);
+//customer.creditcadnumber değeri gelmez çünkü person üstünden erişemeye çaışıyorum
+
+Console.WriteLine(((Customer)person3).CreditCardNumber); //customer bilgisini getirmek için çünkü person3 person üzerinden türetilmişti sadece onun özelliklerine sahiptir.
+
+PersonManager manager = new PersonManager();
+manager.Add(person3);
+manager.Add(customer);
+manager.Add(employee);
+class Person
+{
+
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class Customer :Person
+{
+    public string CreditCardNumber { get; set; }
+}
+
+class Employee :Person
+{
+    public int EmployeeNumber { get; set; }
+}
+
+class PersonManager
+{
+    public void Add(Person person)
+    {
+        Console.WriteLine(person.FirstName); 
+    }
+}
